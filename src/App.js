d8c31b661321e1cc78cbb72d2cf5da81d37f4059
whwhll <whwhll@163.com>
2020-06-02 22:01:21 +0800

    my-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <p className="title">
+        html 
+      </p>
+      <Button type="primary" className="anniu2">按钮</Button>
+    </div>
+  );
+}
+
+export default App;
